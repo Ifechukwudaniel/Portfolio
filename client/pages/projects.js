1be3b75a711bc3bbb9ce48fb0c42d440a53baa1c
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-04 13:35:12 +0100

    finshed project  page   project item

--- client/pages/projects.js
+++ client/pages/projects.js
@@ -1,4 +1,5 @@
 import Layout from "../components/layout"
+import ProjectItem from "../components/Project/ProjectItem"
 import Seo from "../components/seo"
 import { fetchAPI } from "../lib/api"
 
@@ -6,8 +7,15 @@ const Projects = ({ homepage }) => {
   return (
     <Layout categories={[]}>
       <Seo seo={homepage.seo} />
-      <div className="flex flex-row mt-10 ml-20">
-        <h1 className="navTitle text-4xl text-link-yellow"> PROJECTS </h1>
+      <div className="xl:px-20 lg:px-15 md:px-10 sm:px-5 bg-offWhite">
+        <div className="flex flex-col py-5">
+          <h1 className="navTitle text-center text-4xl text-link-yellow ">
+            PROJECTS
+          </h1>
+        </div>
+        <div>
+          <ProjectItem />
+        </div>
       </div>
     </Layout>
   )
