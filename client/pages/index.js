adaf15c136682d1984cf8f75412f7e6792a6f9bd
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-04 13:42:32 +0100

    mad index page responsive

--- client/pages/index.js
+++ client/pages/index.js
@@ -9,16 +9,18 @@ const index = ({ homepage }) => {
     <Layout darkHeader={false}>
       <Seo seo={homepage.seo} />
       <div className=" h-screen home-background">
-        <div className="h-3/4 w-2/5 bg-dark bg-opacity-90 text-offWhite home-card ">
-          <h2 className="text-offWhite home-card-text">Hi My Name is Daniel</h2>
-          <h2 className="text-offWhite home-card-text">
+        <div className="h-3/4 xl:w-2/5 lg:w-2/4 md:w-3/5 sm:w-full  bg-dark bg-opacity-90 text-offWhite home-card ">
+          <h2 className="text-offWhite  text-5xl  home-card-text">
+            Hi My Name is Daniel
+          </h2>
+          <h2 className="text-offWhite text-4xl home-card-text">
             I'm a FullStack web and mobile app developer. And i am a
           </h2>
           <ReactTyped
             loop
             typeSpeed={90}
             backSpeed={90}
-            className="home-card-text"
+            className="text-4xl  home-card-text"
             strings={[
               "NODE JS YODA",
               "REACT NINJA",
@@ -29,7 +31,7 @@ const index = ({ homepage }) => {
             backDelay={1}
             style={{ marginBottom: "10%" }}
           />
-          <h2 className="text-offWhite home-card-text home-card-text-bottom">
+          <h2 className="text-offWhite  text-5xl home-card-text home-card-text-bottom">
             Let's take a look on my work.
           </h2>
         </div>
