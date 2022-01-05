9dc0dcdc3c3c9510a36db5fd93a3ad184bf9d63e
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-05 17:09:43 +0100

    Project component mad dynamic

--- client/components/Project/ProjectItem.js
+++ client/components/Project/ProjectItem.js
@@ -1,29 +1,40 @@
 import React from "react"
 import Image from "next/image"
+import { getStrapiMedia } from "../../lib/media"
 
-export default function ProjectItem() {
+export default function ProjectItem({
+  title,
+  description,
+  isOpenSource,
+  image,
+}) {
+  const { url, alternativeText, width } = image
+
+  const loader = () => {
+    return `${getStrapiMedia(image)}?w=${width}`
+  }
+
   return (
     <div className=" flex xl:flex-row lg:flex-row md:flex-col-reverse sm:flex-col-reverse  p-4 mt-7  border border-dark">
-      <div className="xl:basis-8/12 lg:basic-2/3  md:basis-2/3">
-        <h1 className="navTitle  text-4xl text-link-yellow">Crown clothing</h1>
-        <p className="Poppins-Medium text-left  text-lg pt-10 pr-20">
-          A full fledge ecommerce website built with react, firebase, redux with
-          redux-sagas to handle asynchronous actions. It is also progressive web
-          app. It is built following proven best practices with redux and redux
-          sagas so can scale well
+      <div className="xl:basis-6/12 lg:basic-6/12  md:basis-2/3">
+        <h1 className="xl:text-left  lg:text-left md:text-left sm:text-center navTitle  text-4xl text-link-yellow">
+          {title}
+        </h1>
+        <p className="xl:w-10/12 lg:w-10/12 Poppins-Medium xl:text-left  lg:text-left md:text-left sm:text-center text-lg pt-10 ">
+          {description}
         </p>
-        <div className="flex flex-row mt-10">
-          <a className="py-2 px-4 bg-white drop-shadow-md  hover:drop-shadow-2xl text-dark border border-dark itim">
+        <div className="flex flex-row mt-10 ">
+          <div className="py-2 px-4 bg-white drop-shadow-md  hover:drop-shadow-2xl text-dark border border-dark itim">
             See Live
-          </a>
-          <a className="py-2 px-4 bg-white drop-shadow-md hover:drop-shadow-2xl text-dark border border-dark itim ml-20">
+          </div>
+          <div className="py-2 px-4 bg-white drop-shadow-md hover:drop-shadow-2xl text-dark border border-dark itim">
             Source Code
-          </a>
+          </div>
         </div>
       </div>
-      <div className="xl:basis-4/12 lg:basic-1/3  md:basis-1/3">
-        <div className="">
-          <Image src="/images/png/crowncl.png" width="300" height="200" />
+      <div className="xl:basis-6/12 lg:basic-6/12  md:basis-1/3">
+        <div className=" ml-10 ">
+          <Image loader={loader} src={url} width="580" height="300" />
         </div>
       </div>
     </div>
