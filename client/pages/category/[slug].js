89034d8aa3209beca23858d2f2693943f8d62dd5
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-05 17:11:41 +0100

    Finished category page

--- client/pages/category/[slug].js
+++ client/pages/category/[slug].js
@@ -1,20 +1,47 @@
-import Articles from "../../components/Blogs/Articles"
 import { fetchAPI } from "../../lib/api"
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
-import { CategoryItem } from "../../components/Blogs/Categories"
+import { CategoryButton, CategoryItem } from "../../components/Blogs/Categories"
+import BlogCard from "../../components/Blogs/BlogCard"
+import { sampleSize } from "lodash"
 
-const Category = ({ category, categories }) => {
+const Category = ({ category, categories, articles }) => {
   const seo = {
     metaTitle: category.name,
     metaDescription: `All ${category.name} articles`,
   }
 
+  let categoryShuffle = sampleSize(categories, 10)
+  console.log(categoryShuffle)
+
   return (
-    <Layout categories={categories}>
+    <Layout>
       <Seo seo={seo} />
-      <CategoryItem categoryName={category.name} />
-      <Articles articles={category.articles} />
+      <div className="flex flex-row">
+        <div
+          style={{ background: "#F7ECC9", minHeight: "90vh" }}
+          className=" xl:w-8/12  lg:w-8/12  bg-link-yellow overflow-y-auto xl:pl-16 md:pl-16  px-3 pt-8"
+        >
+          <div className="w-5/12 mb-6">
+            <CategoryItem category={category} />
+          </div>
+          {articles.map((article) => (
+            <BlogCard hideCategory={true} article={article} />
+          ))}
+        </div>
+        <div className="overflow-y-auto">
+          <div className=" xl:w-4/12  lg:w-4/12  p-5 hidden-menu fixed ">
+            <div className="mt-10  ">
+              <h1 className="Poppins-Medium text-2xl"> Recommended topics</h1>
+            </div>
+            <div className="flex flex-wrap mt-5 ">
+              {categoryShuffle.map((category) => (
+                <CategoryButton category={category} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
@@ -33,11 +60,12 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const category = (await fetchAPI(`/categories?slug=${params.slug}`))[0]
+  const category = await fetchAPI(`/categories/${params.slug}`)
   const categories = await fetchAPI("/categories")
+  const articles = category.articles
 
   return {
-    props: { category, categories },
+    props: { category, categories, articles },
     revalidate: 1,
   }
 }
