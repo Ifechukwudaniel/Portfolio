968e1917a8344777c9310734ca50d47f529b4e64
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-05 14:47:34 +0100

    Finished Category components

--- client/components/Blogs/Categories.js
+++ client/components/Blogs/Categories.js
@@ -1,35 +1,20 @@
 import React from "react"
 import Link from "next/link"
 
-export function CategoryItem({ categoryName }) {
+export function CategoryItem({ category }) {
   return (
-    <div className="flex flex-row mr-10 mt-10 ml-10 mb-6">
-      <h1 className=" basis-1/5 navTitle text-4xl text-link-yellow">
-        {categoryName}
-      </h1>
+    <div className=" text-2xl py-4 px-4 ml-0 mr-4 m-3 bg-white  text-dark border border-dark itim">
+      {category.name}
     </div>
   )
 }
 
-function Category({ category }) {
+export function CategoryButton({ category }) {
   return (
     <Link href={`/category/${category.slug}`}>
-      <a className="ml-10">
-        <h1 className="navTitle text-xl Poppins-Bold">{category.name}</h1>
-      </a>
-    </Link>
-  )
-}
-
-export default function Categories({ categories }) {
-  return (
-    <div className="flex flex-row mr-10 mt-10 ml-10 mb-6">
-      <h1 className=" basis-1/5 navTitle text-3xl text-link-yellow">Blogs</h1>
-      <div className="flex flex-row basis-4/5 justify-end text-white">
-        {categories.map((category, i) => (
-          <Category key={i} category={category} />
-        ))}
+      <div className=" text-2xl py-4 px-4 ml-0 mr-4 m-3 bg-white drop-shadow-md hover:drop-shadow-2xl text-dark border border-dark itim">
+        {category.name}
       </div>
-    </div>
+    </Link>
   )
 }
