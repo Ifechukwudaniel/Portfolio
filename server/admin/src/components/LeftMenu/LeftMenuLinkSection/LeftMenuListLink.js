f518657234d96df4fed93cf3d96988487367207d
Angry Coding <mailidanielifechukwu@gmail.com>
2022-09-27 10:38:35 +0100

    removed backend

--- server/admin/src/components/LeftMenu/LeftMenuLinkSection/LeftMenuListLink.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const LeftMenuListLink = styled.div`
-  margin-bottom: 19px;
-  margin-right: 25px;
-  overflow: auto;
-`;
-
-export default LeftMenuListLink;
--- server/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '662beec1-e5e7-41c4-a0a2-d2cd886ef0e5'
-};
